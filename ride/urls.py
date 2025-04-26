329d9a436fa8a114d9ba538c2bea9ca45da53862
shariqmunir99 <shariq.munir7@gmail.com>
2025-04-26 14:09:10 +0500

    change RideHistory file organization

--- ride/urls.py
+++ ride/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RideViewSet, RideHistoryListApiView
+from .views import RideViewSet
 
 router = DefaultRouter()
 router.register(r'', RideViewSet, basename='ride')
 
 urlpatterns = [
-  path('history/', RideHistoryListApiView.as_view()),
   path('', include(router.urls)),
 ]
