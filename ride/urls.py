0131bb978225479d99afd93be256201d76366a73
shariqmunir99 <shariq.munir7@gmail.com>
2025-05-01 14:14:20 +0500

    add search/ to urls.py

--- ride/urls.py
+++ ride/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RideViewSet
+from .views import RideViewSet, RideSearchApiView
 
 router = DefaultRouter()
 router.register(r'', RideViewSet, basename='ride')
 
 urlpatterns = [
+  path('search/', RideSearchApiView.as_view(), name='ride-search'),
   path('', include(router.urls)),
 ]
